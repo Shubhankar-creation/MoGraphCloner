e7110d4a65fc668b6a16f36322ff94d682559b21
Shubhankar-creation <shubhankarsharma0135@gmail.com>
2022-04-03 11:52:37 +0530

    Initial Commit

---
+++ Assets/Scripts/Cloner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloner : MonoBehaviour
+{
+    [Range(0, 100)]
+    public int count;
+
+    public PositionSetting positionSetting;
+    public ScaleSetting scaleSetting;
+    public RotationSetting rotationSetting;
+    public StepRotationSetting stepRotationSetting;
+    public RadiusSetting radiusSetting;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
